360833daf7d5daeeb0d6eb94751da301eda800e5
JHL <osjin6633@gmail.com>
2021-05-27 00:47:05 +0900

    자동매매 부분 ui에서 일단 삭제

--- main.py
+++ main.py
@@ -11,28 +11,6 @@ class MainWindow(QMainWindow, form_class):
         super().__init__()
         self.setupUi(self)
 
-        self.button.clicked.connect(self.clickBtn)
-
-    def clickBtn(self):
-        if self.button.text() == "매매시작":
-            apiKey = self.apiKey.text()
-            secKey = self.secKey.text()
-            if len(apiKey) != 32 or len(secKey) != 32:
-                self.textEdit.append("KEY가 올바르지 않습니다.")
-                return
-            else:
-                self.b = pyupbit.Upbit(apiKey, secKey)
-                balance = self.b.get_balance(self.ticker)
-                if balance == None:
-                    self.textEdit.append("KEY가 올바르지 않습니다.")
-                    return
-            self.button.setText("매매중지")
-            self.textEdit.append("------ START ------")
-            self.textEdit.append(f"보유 현금 : {self.balance[1]} 원")
-        else:
-            self.textEdit.append("------- END -------")
-            self.button.setText("매매시작")
-
     def closeEvent(self, event):
         self.widget.closeEvent(event)
         self.widget_2.closeEvent(event)
