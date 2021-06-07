8e54c49bfbe5c33d12fcf48b6c79df157f6e75ce
ucs0191@naver.com <ucs0191@naver.com>
2021-06-08 01:26:16 +0900

    캔들차트 test 모듈 수정

--- test.py
+++ test.py
@@ -4,34 +4,41 @@ from PyQt5.QtGui import *
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from mplfinance.original_flavor import candlestick2_ohlc
+import matplotlib.ticker as ticker
 import pyupbit
 
 class MyWindow(QWidget):
     def __init__(self):
         super().__init__()
         self.setupUI()
-        self.pushButtonClicked()
+        self.candle_stick()
 
     def setupUI(self):
         self.setGeometry(600, 200, 1200, 600)
-        self.setWindowTitle("PyChart Viewer v0.1")
-        self.setWindowIcon(QIcon('icon.png'))
+        self.setWindowTitle("chart")
 
         self.fig = plt.Figure()
         self.canvas = FigureCanvas(self.fig)
 
-        leftLayout = QHBoxLayout()
-        leftLayout.addWidget(self.canvas)
-
-        layout = QHBoxLayout()
-        layout.addLayout(leftLayout)
-        layout.setStretchFactor(leftLayout, 1)
+        layout = QVBoxLayout()
+        layout.addWidget(self.canvas)
 
         self.setLayout(layout)
 
-    def pushButtonClicked(self):
-        df = pyupbit.get_ohlcv("KRW-ETH", count=30)
+    def candle_stick(self):
+        df = pyupbit.get_ohlcv("KRW-BTC", count=30)
+
         ax = self.fig.add_subplot(111)
+
+        day_list = []
+        name_list = []
+        for i, day in enumerate(df.index):
+            if day.dayofweek == 0:
+                day_list.append(i)
+                name_list.append(day.strftime('%Y-%m-%d') + '(Mon)')
+
+        ax.xaxis.set_major_locator(ticker.FixedLocator(day_list))
+        ax.xaxis.set_major_formatter(ticker.FixedFormatter(name_list))
 
         candlestick2_ohlc(ax, df['open'], df['high'], df['low'], df['close'], width=0.5, colorup='r', colordown='b')
 
