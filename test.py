e3d12d5f344405f8b67fb75f7f2db466edd0f7f7
ucs0191@naver.com <ucs0191@naver.com>
2021-06-08 00:18:23 +0900

    test 모듈에 임시 캔들차트 구현

--- test.py
+++ test.py
@@ -1,1 +1,46 @@
-print("Hello, BTC!")
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from mplfinance.original_flavor import candlestick2_ohlc
+import pyupbit
+
+class MyWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setupUI()
+        self.pushButtonClicked()
+
+    def setupUI(self):
+        self.setGeometry(600, 200, 1200, 600)
+        self.setWindowTitle("PyChart Viewer v0.1")
+        self.setWindowIcon(QIcon('icon.png'))
+
+        self.fig = plt.Figure()
+        self.canvas = FigureCanvas(self.fig)
+
+        leftLayout = QHBoxLayout()
+        leftLayout.addWidget(self.canvas)
+
+        layout = QHBoxLayout()
+        layout.addLayout(leftLayout)
+        layout.setStretchFactor(leftLayout, 1)
+
+        self.setLayout(layout)
+
+    def pushButtonClicked(self):
+        df = pyupbit.get_ohlcv("KRW-ETH", count=30)
+        ax = self.fig.add_subplot(111)
+
+        candlestick2_ohlc(ax, df['open'], df['high'], df['low'], df['close'], width=0.5, colorup='r', colordown='b')
+
+        ax.grid()
+
+        self.canvas.draw()
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MyWindow()
+    window.show()
+    app.exec_()
